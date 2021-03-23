ebf3a81576d138f140db76688be92e114febf3e3
Vy4thewin <vya.hill@gmail.com>
2021-03-23 00:01:43 -0400

    Update index.js
    New JS for Index, cannot combined all html files into one JS

--- index.js
+++ index.js
@@ -1,26 +1,56 @@
-/*Javascript for ESUM
-    Created by Vyanna Hill
-    Please comment around your work with your name
+/*Javascript for Esum
+Created by Vyanna Hill
+Give functionality to the button of Faq page
 */
 
-// Button functionality, Vyanna
-var visText= document.getElementById("more");
-var button=document.getElementById("btn");
-visText.style.display="none";
-//We set the instructions to invisible until user clicks button
 
-document.getElementById("btn").onclick=function(){
-    if(button.innerHTML=="See More"){
-        visText.style.display="inline";
-        button.innerHTML="See Less";
+var btnSign=document.getElementById("btnLog");
+var label=document.getElementById("label");
+var eMail=document.getElementById("email");
+var btnC=document.getElementById("btnCC");
+var btn1=document.getElementById("btn1t1");
+var btnOrg=document.getElementById("btnOrg");
+var btnDel=document.getElementById("btnDel");
+var welcome=document.getElementById("welcome");
+/// Any ids used in the aside function
+
+btn1.style.display="none";
+btnOrg.style.display="none";
+btnC.style.display="none";
+btnDel.style.display="none";
+//hide all the log in options 
+
+btnSign.addEventListener("click",function(){
+    //Need a if statment that checks , email is used AND in database. else, send a alert to page
+
+    if(btnSign.innerHTML=="Log In"){
+        eMail.style.display="none";
+        btnSign.innerHTML="Log out";
+        label.style.display="none";
+        welcome.innerHTML="Hello, "+eMail.value;
+            // Greet logged in User with database options, hide the log information 
+
+        btnC.style.display="inLine";
+        btn1.style.display="inLine";
+        btnOrg.style.display="inLine";
+        btnDel.style.display="inLine";
+            //options for a Admin user will appear
     }else{
-        visText.style.display="none";
-        button.innerHTML="See More";
+        label.style.display="inLine";
+        btn1.style.display="none";
+        btnOrg.style.display="none";
+        btnC.style.display="none";
+        btnDel.style.display="none";
+        //When user "signs out", Hide logged in functions and "return" to home page
+
+        welcome.innerHTML="";
+        eMail.style.display="inLine";
+        btnSign.innerHTML="Log In";
+
     }
-    //When User clicks button the first time, Instructions are revealed and button text is rewritten to See less
-    //When the User want to hide them again, the click will hide them and See More appears on the button again
-}
-//Button Func, Vyanna
+   
+
+});
 
 
-// Functionality for Sign in. It will take User's input and create a query for User's info
+
