e8e36834939ce2b5e27bdb7adc17a18b53c10be2
Vy4thewin <vya.hill@gmail.com>
2021-03-23 22:37:19 -0400

    commit on 'main'

--- index.js
+++ index.js
@@ -1,56 +1,77 @@
 /*Javascript for Esum
-Created by Vyanna Hill
-Give functionality to the button of Faq page
+By Vyanna Hill, Braddy Madden
+  VY:  Gave functionality to the button of login, when clicked log in button is click, the home screen appears
 */
 
 
-var btnSign=document.getElementById("btnLog");
-var label=document.getElementById("label");
-var eMail=document.getElementById("email");
-var btnC=document.getElementById("btnCC");
-var btn1=document.getElementById("btn1t1");
-var btnOrg=document.getElementById("btnOrg");
-var btnDel=document.getElementById("btnDel");
-var welcome=document.getElementById("welcome");
-/// Any ids used in the aside function
+//Cleaned up, gave better var names and gave functionality to added buttons, Brady Madden
 
-btn1.style.display="none";
-btnOrg.style.display="none";
-btnC.style.display="none";
-btnDel.style.display="none";
-//hide all the log in options 
 
-btnSign.addEventListener("click",function(){
-    //Need a if statment that checks , email is used AND in database. else, send a alert to page
+var btnSign = document.getElementById("btnLog");//Login Button
+var welcome = document.getElementById("welcome");//sucessful login label
+var forgotpassbtn = document.getElementById("ForgotPass");//Forgot Password Button
+var eLabel = document.getElementById("elabel");//Labels for email and pass
+var pLabel = document.getElementById("plabel");
+var eMail = document.getElementById("email");
+var pass = document.getElementById("password");
 
-    if(btnSign.innerHTML=="Log In"){
-        eMail.style.display="none";
-        btnSign.innerHTML="Log out";
-        label.style.display="none";
-        welcome.innerHTML="Hello, "+eMail.value;
-            // Greet logged in User with database options, hide the log information 
+var btnC = document.getElementById("btnCC");
+var btn1 = document.getElementById("btn1t1");
+var btnOrg = document.getElementById("btnOrg");
+var btnDel = document.getElementById("btnDel");
+//Buttons for chat nav.
 
-        btnC.style.display="inLine";
-        btn1.style.display="inLine";
-        btnOrg.style.display="inLine";
-        btnDel.style.display="inLine";
-            //options for a Admin user will appear
-    }else{
-        label.style.display="inLine";
-        btn1.style.display="none";
-        btnOrg.style.display="none";
-        btnC.style.display="none";
-        btnDel.style.display="none";
-        //When user "signs out", Hide logged in functions and "return" to home page
+//Dont Display these buttons when not logged in
+btn1.style.display = "none";
+btnOrg.style.display = "none";
+btnC.style.display = "none";
+btnDel.style.display = "none";
 
-        welcome.innerHTML="";
-        eMail.style.display="inLine";
-        btnSign.innerHTML="Log In";
 
-    }
+welcome.innerHTML = "Sign In";
+plabel.style.display = "inLine";
+//add login event
+
+btnSign.addEventListener("click", function () {
+    //Needs a if statment that checks email with a find query. if email is used AND in database, continue on home page.
+    // else, send a alert to webpage
    
+   
+    if (btnSign.innerHTML == "Login") {
+        //When logged in, hide all login info and display buttons
+        forgotpassbtn.style.display = "none";
+        eMail.style.display = "none";
+        pass.style.display = "none";
+        btnSign.innerHTML = "Logout";
+        eLabel.style.display = "none";
+        pLabel.style.display = "none";
+        
 
+        // Greet logged in User with database options
+        welcome.innerHTML = "Hello, " + eMail.value;
+        btnC.style.display = "inLine";
+        btn1.style.display = "inLine";
+        btnOrg.style.display = "inLine";
+        btnDel.style.display = "inLine";
+        //These options above are all options for admin user.
+        //Later on when contected, Flied of isAdmin will be check to block non admins option of delete
+
+    } else {
+        //When user "signs out", Hide logged in functions and "return" to home page
+        eMail.style.display = "inLine";
+        pass.style.display = "inLine";
+        eLabel.style.display = "inLine";
+        pLabel.innerHTML = "Password:";
+        pLabel.style.display = "inLine";
+        welcome.innerHTML = "Sign In";
+        forgotpassbtn.style.display = "inLine";
+        btnSign.innerHTML = "Login";
+        
+        btn1.style.display = "none";
+        btnOrg.style.display = "none";
+        btnC.style.display = "none";
+        btnDel.style.display = "none";
+    }
 });
-
 
 
